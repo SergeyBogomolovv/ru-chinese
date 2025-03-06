480374d03c99af622b8bf4d4824dc9e6ea76fab2
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-03-06 20:34:45 +0300

    fix: add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import InfoCard from '@/components/info-card/card'
 import SearchForm from '@/components/search-form/form'
 import { useSearch } from '@/api/search'
+import { Suspense } from 'react'
 
 export default function Home() {
   const searched = useSearch()
@@ -12,22 +13,24 @@ export default function Home() {
           Толковый русско-китайский строительный словарь
         </h1>
       </header>
-      <section className='flex flex-col gap-4 max-w-[600px] mx-auto p-4'>
-        <SearchForm />
-        {searched.map(({ item }, i) => (
-          <InfoCard
-            key={i}
-            data={{
-              title: item.title,
-              description: item.description,
-              description_ch: item.description_ch,
-              transcription: item.transcription,
-              image: item.image.replace('/public', ''),
-              translation: item.translation,
-            }}
-          />
-        ))}
-      </section>
+      <Suspense>
+        <section className='flex flex-col gap-4 max-w-[600px] mx-auto p-4'>
+          <SearchForm />
+          {searched.map(({ item }, i) => (
+            <InfoCard
+              key={i}
+              data={{
+                title: item.title,
+                description: item.description,
+                description_ch: item.description_ch,
+                transcription: item.transcription,
+                image: item.image.replace('/public', ''),
+                translation: item.translation,
+              }}
+            />
+          ))}
+        </section>
+      </Suspense>
     </main>
   )
 }
