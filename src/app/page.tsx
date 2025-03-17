e30ab31d08bba2d7aa347108afd86ede0d709b37
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-03-17 12:02:32 +0300

    feat: update header and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ import logo from '@/assets/logo-white.svg'
 export default function Home() {
   return (
     <div className='flex flex-col min-h-screen'>
-      <header className='p-6 md:p-8 lg:p-10 bg-[#1B365D] flex flex-col justify-center gap-8 md:gap-10 lg:gap-12'>
-        <Image src={logo} alt='logo' className='w-50 md:w-60' />
+      <header className='p-6 md:p-8 lg:p-10 bg-[#1B365D] flex flex-col-reverse md:flex-row justify-center md:justify-between gap-8 md:gap-10 lg:gap-12'>
         <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold text-white'>
           Толковый русско-китайский <br /> строительный словарь
         </h1>
+        <Image src={logo} alt='logo' className='w-50 md:w-60' />
       </header>
 
       <main className='flex flex-col gap-4 max-w-full w-[600px] mx-auto p-4 flex-1'>
@@ -25,12 +25,12 @@ export default function Home() {
 
       <footer className='bg-[#101820] p-6 gap-2 text-white flex items-center justify-between flex-col sm:flex-row'>
         <FeedbackDialog lang='ru'>
-          <Button variant='ghost' className='text-md'>
+          <Button variant='ghost' className='text-md border-2 w-full sm:w-fit border-gray-700'>
             Есть вопросы? Нажмите сюда
           </Button>
         </FeedbackDialog>
         <FeedbackDialog lang='zh'>
-          <Button variant='ghost' className='text-md'>
+          <Button variant='ghost' className='text-md border-2 w-full sm:w-fit border-gray-700'>
             有什麼問題嗎？點這裡
           </Button>
         </FeedbackDialog>
