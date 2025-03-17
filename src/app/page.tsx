7895c8cd9831b3983971b85df3cbdc770d1731ee
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-03-17 12:44:57 +0300

    feat: update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,22 @@ import SearchWithSuggestions from '@/components/search-form'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { Suspense } from 'react'
-import logo from '@/assets/logo-white.svg'
+import logo from '@/assets/logo.svg'
+import logo_wide from '@/assets/logo-wide.svg'
 
 export default function Home() {
   return (
     <div className='flex flex-col min-h-screen'>
-      <header className='p-6 md:p-8 lg:p-10 bg-[#1B365D] flex flex-col-reverse md:flex-row justify-center md:justify-between gap-8 md:gap-10 lg:gap-12'>
-        <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold text-white'>
+      <header className='p-4 md:p-8 lg:p-10 bg-[#1B365D] flex justify-between gap-2 md:gap-10 lg:gap-12'>
+        <h1 className='lg:text-4xl sm:text-3xl text-md font-bold text-white'>
           Толковый русско-китайский <br /> строительный словарь
         </h1>
-        <Image src={logo} alt='logo' className='w-50 md:w-60' />
+        <Image src={logo} alt='logo' className='md:hidden sm:w-20 w-10' />
+        <Image
+          src={logo_wide}
+          alt='logo'
+          className='hidden md:block w-30 sm:w-50 md:w-60 lg:w-70'
+        />
       </header>
 
       <main className='flex flex-col gap-4 max-w-full w-[600px] mx-auto p-4 flex-1'>
